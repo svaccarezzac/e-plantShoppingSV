2eed86adbaa641cad88fda6229fc74ae2e76762e
svaccarezzac <svaccarezzac@gmail.com>
2025-05-10 20:01:24 -0600

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
+// redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import cartReducer from '../CartSlice'; // Adjust path if CartSlice is in a different folder
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
